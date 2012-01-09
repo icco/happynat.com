aba95a8093d3e046df6d6f1e5a17dd1dd366cd96
Nat Welch <nat@natwelch.com>
2012-01-08 17:37:59 -0800

    time.humanize

--- site.rb
+++ site.rb
@@ -60,3 +60,16 @@ end
 
 class Entry < Sequel::Model(:entries)
 end
+
+# Nice time printing
+class Time
+  def humanize
+    if Time.now.strftime("%F") == self.strftime("%F")
+      return self.strftime("%l:%M %P")
+    elsif Time.now.year == self.year
+      return self.strftime("%b %e")
+    else
+      return self.strftime("%b %e '%y")
+    end
+  end
+end
