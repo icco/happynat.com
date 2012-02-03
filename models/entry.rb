633f24c33aef3e4afa5eaa9bc65323d21f688a11
Nat Welch <nat@natwelch.com>
2012-02-02 23:07:13 -0800

    return returns. Duh.

--- models/entry.rb
+++ models/entry.rb
@@ -1,15 +1,9 @@
 class Entry < Sequel::Model(:entries)
   def html
     html = self.text.split(" ").map do |text|
-      parsed = UrlParser.getUrls(text).map do |url|
+      UrlParser.getUrls(text).map do |url|
         UrlParser.transformUrl(url)
-      end.join("")
-
-      if parsed.empty?
-        return text
-      else
-        return parsed
-      end
+      end.join("") or text
     end.join(" ")
 
     return "<p>#{html}</p>"
