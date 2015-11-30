5f188d08d474212be18a6b0ab87d105cc15ef1e7
Nat Welch <nat@natwelch.com>
2015-11-30 03:32:29 +0000

    comments

--- site.rb
+++ site.rb
@@ -5,13 +5,11 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
 
 configure do
   RACK_ENV = (ENV['RACK_ENV'] || :development).to_sym
-  p RACK_ENV
   connections = {
     :development => "postgres://localhost/happynat",
     :test => "postgres://postgres@localhost/happynat_test",
     :production => ENV['DATABASE_URL']
   }
-  p connections
 
   url = URI(connections[RACK_ENV])
   options = {
@@ -50,6 +48,28 @@ end
 post "/sms" do
   #@client = Twilio::REST::Client.new
   p params
+  # {
+  #   "ToCountry"=>"US",
+  #   "ToState"=>"NY",
+  #   "SmsMessageSid"=>"SM7b7271dd550ee8340eb40ef296325ce7",
+  #   "NumMedia"=>"0",
+  #   "ToCity"=>"",
+  #   "FromZip"=>"95403",
+  #   "SmsSid"=>"SM7b7271dd550ee8340eb40ef296325ce7",
+  #   "FromState"=>"CA",
+  #   "SmsStatus"=>"received",
+  #   "FromCity"=>"SANTA ROSA",
+  #   "Body"=>"test",
+  #   "FromCountry"=>"US",
+  #   "To"=>"+16468762600",
+  #   "MessagingServiceSid"=>"MGec57cdb198f67ca5720b77bc2b395c56",
+  #   "ToZip"=>"",
+  #   "NumSegments"=>"1",
+  #   "MessageSid"=>"SM7b7271dd550ee8340eb40ef296325ce7",
+  #   "AccountSid"=>"ACd13bade3c51c7c83670737a310dc790a",
+  #   "From"=>"+17077998675",
+  #   "ApiVersion"=>"2010-04-01"
+  # }
   response = Twilio::TwiML::Response.new do |r|
     r.Message 'hello there'
   end
